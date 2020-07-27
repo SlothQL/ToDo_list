a14c71bd022435ed49087419000c8764b726f63b
Alina <lindenalina.al@gmail.com>
2020-07-27 15:10:15 +0100

    add new to dos to list passed

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
         data: {
-            todos: ['Buy groceries', 'Clean bathroom', 'Hoover floor']
+            todos: ['Buy groceries', 'Clean bathroom', 'Hoover floor'],
+            newTodo: ''
+        },
+        methods: {
+            saveNewTodo: function() {
+                this.todos.push(this.newTodo);
+            }
         }
-
-
     });
 });
